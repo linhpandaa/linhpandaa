ae2d7024c74e1b5e80841f75b0369f0374fa9f6f
linhpandaa <phuonglinh992003@gmail.com>
2022-04-29 21:52:52 +0300

    update hw7.2

--- hw7.2.cpp
+++ hw7.2.cpp
@@ -1,54 +1,67 @@
 #include <iostream>
 using namespace std;
 
-struct fraction {
+struct fraction
+{
     int a;
-    int b;  // check b != 0
-    bool valid() {
-        return b != 0;
-    }
-    void optimize() {
-        // only valid fraction
-        int _a = (a > 0)? (a) : (-a);
-        int _b = (b > 0)? (b) : (-b);
-
-        // gcd
-        while (_a != 0 && _b != 0) {
-            if (_a < _b)
-                _b %= _a;
-            else 
-                _a %= _b; 
-        }
-        
-        int c = _a + _b;    // because one of them is 0
-        a /= c;
-        b /= c;
-    }
-    void str() {
-        if (a == 0)
-            cout << a << endl;
-        else {
-            if ((a < 0 && b > 0) || (a > 0 && b < 0)) {
-                cout << "-";
-            }
-            cout << ((a > 0)? (a) : (-a)) << "/";
-            cout << ((b > 0)? (b) : (-b)) << endl;
-        }
-    }
+    int b;
 };
 
-int main () {
+bool valid(fraction &f)
+{
+    return f.b != 0;
+}
+
+void optimize(fraction &f)
+{
+    // only valid fraction
+    int _a = (f.a > 0) ? (f.a) : (-f.a);
+    int _b = (f.b > 0) ? (f.b) : (-f.b);
+
+    // gcd
+    while (_a != 0 && _b != 0)
+    {
+        if (_a < _b)
+            _b %= _a;
+        else
+            _a %= _b;
+    }
+
+    int c = _a + _b; // because one of them is 0
+    f.a /= c;
+    f.b /= c;
+}
+
+void disp(const fraction &f)
+{
+    if (f.a == 0)
+        cout << f.a << endl;
+    else
+    {
+        if ((f.a < 0 && f.b > 0) || (f.a > 0 && f.b < 0))
+        {
+            cout << "-";
+        }
+        cout << ((f.a > 0) ? (f.a) : (-f.a)) << "/";
+        cout << ((f.b > 0) ? (f.b) : (-f.b)) << endl;
+    }
+}
+
+int main()
+{
     cout << "Input fraction: ";
     int a, b;
     cin >> a >> b;
 
     fraction x = {a, b};
-    if (x.valid()) {
-        x.optimize();
-        x.str();
-    } else {
+    if (valid(x))
+    {
+        optimize(x);
+        disp(x);
+    }
+    else
+    {
         cout << "Error: Invalid fraction." << endl;
     }
     return 0;
 }
-
